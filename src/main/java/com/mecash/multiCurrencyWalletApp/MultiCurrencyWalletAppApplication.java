c16b7dfc3f1c1f010858f8ec2972d341481ffd47
Emeka Ukwuoma <emekaukwuoma546@gmail.com>
2025-01-09 22:24:34 +0100

    Initial Project Set up commit

---
+++ src/main/java/com/mecash/multiCurrencyWalletApp/MultiCurrencyWalletAppApplication.java
@@ -0,0 +1,13 @@
+package com.mecash.multiCurrencyWalletApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCurrencyWalletAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCurrencyWalletAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mecash/multiCurrencyWalletApp/MultiCurrencyWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecash.multiCurrencyWalletApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCurrencyWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
